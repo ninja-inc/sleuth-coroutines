f0a446db149e06de51fc25198df4c5cc67d62b19
takaaki.iida <takiida0605@gmail.com>
2020-09-12 20:48:02 +0900

    init

---
+++ src/main/kotlin/com/example/sleuthcoroutines/SleuthCoroutinesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sleuthcoroutines
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SleuthCoroutinesApplication
+
+fun main(args: Array<String>) {
+	runApplication<SleuthCoroutinesApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/sleuthcoroutines/SleuthCoroutinesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sleuthcoroutines
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SleuthCoroutinesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
